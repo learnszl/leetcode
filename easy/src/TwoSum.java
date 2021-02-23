31c259b2853a4765f0f22c1c1902727f0ec7af9e
learnszl <ahsongzhili@163.com>
2021-02-23 20:32:07 +0800

    两数之和

--- easy/src/TwoSum.java
+++ easy/src/TwoSum.java
@@ -6,12 +6,12 @@ import java.util.HashMap;
  * @Description
  */
 public class TwoSum {
-    public static int[] twoSum(int[] nums, int target){//暴力破解
-        int[]  ans = new int[2];
+    public static int[] twoSum(int[] nums, int target) {//暴力破解
+        int[] ans = new int[2];
         int N = nums.length;
-        for (int i = 0; i < N-1 ; i++) {
-            for (int j = i+1; j < N; j++) {
-                if(nums[i] + nums[j] == target){
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (nums[i] + nums[j] == target) {
                     ans[0] = i;
                     ans[1] = j;
                 }
@@ -21,22 +21,23 @@ public class TwoSum {
         }
         return ans;
     }
-    public static int[] twoSum1(int[] nums, int target){//哈希表
-        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
+    public static int[] twoSum1(int[] nums, int target) {//哈希表
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
         int N = nums.length;
         for (int i = 0; i < N; i++) {
             if (hashMap.containsKey(target - nums[i]))
-                return new int[]{hashMap.get(target-nums[i]), i};
-            hashMap.put(nums[i],i);
-            
+                return new int[]{hashMap.get(target - nums[i]), i};
+            hashMap.put(nums[i], i);
+
         }
         return new int[0];
 
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,5,5,28};
-        for (int value: twoSum1(nums,10)){
+        int[] nums = {1, 5, 5, 28};
+        for (int value : twoSum1(nums, 10)) {
             System.out.println(value);
         }
     }
