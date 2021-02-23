0f053c295a1dff848d217296c3f7922078d3c3be
learnszl <ahsongzhili@163.com>
2021-02-23 20:30:44 +0800

    两数之和

--- easy/src/TwoSum.java
+++ easy/src/TwoSum.java
@@ -1,4 +1,4 @@
-
+import java.util.HashMap;
 
 /**
  * @Author szl
@@ -6,7 +6,7 @@
  * @Description
  */
 public class TwoSum {
-    public static int[] twoSum(int[] nums, int target){
+    public static int[] twoSum(int[] nums, int target){//暴力破解
         int[]  ans = new int[2];
         int N = nums.length;
         for (int i = 0; i < N-1 ; i++) {
@@ -20,7 +20,25 @@ public class TwoSum {
 
         }
         return ans;
-
+    }
+    public static int[] twoSum1(int[] nums, int target){//哈希表
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        int N = nums.length;
+        for (int i = 0; i < N; i++) {
+            if (hashMap.containsKey(target - nums[i]))
+                return new int[]{hashMap.get(target-nums[i]), i};
+            hashMap.put(nums[i],i);
+            
+        }
+        return new int[0];
 
     }
+
+    public static void main(String[] args) {
+        int[] nums = {1,5,5,28};
+        for (int value: twoSum1(nums,10)){
+            System.out.println(value);
+        }
+    }
+
 }
