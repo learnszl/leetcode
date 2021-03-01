c4647293cd0db500692f62c80e87489931dffb1b
learnszl <ahsongzhili@163.com>
2021-03-01 21:27:08 +0800

    最长回文子串

--- medium/src/LongestPalindrome.java
+++ medium/src/LongestPalindrome.java
@@ -11,7 +11,7 @@ public class LongestPalindrome {
             return s;
         int start = 0;
         int end = 0;
-        int len ;
+        int len;
         for (int i = 0; i < N; i++) {
             int lenodd = expandCenter(s, i, i);
             int leneven = expandCenter(s, i, i + 1);
@@ -38,15 +38,47 @@ public class LongestPalindrome {
 
     public String longestPalindrome1(String s) {//动态规划
         int N = s.length();
-        if(N < 2)
+        if (N < 2)
             return s;
         boolean[][] dp = new boolean[N][N];
-        String ans = "";
-        return ans;
+        int start = 0;
+        int maxlen = 1;
+        for (int i = 0; i < N; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < N; j++) {
+            for (int i = 0; i < j; i++) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    dp[i][j] = false;
+
+                } else {
+                    if (j - i < 3)
+                        dp[i][j] = true;
+                    else
+                        dp[i][j] = dp[i + 1][j - 1];
+
+                }
+                if (dp[i][j] && j - i + 1 > maxlen) {
+                    maxlen = j - i + 1;
+                    start = i;
+                }
+            }
+
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(start + " " + maxlen);
+        return s.substring(start, start + maxlen);
+
+
     }
 
     public static void main(String[] args) {
         LongestPalindrome longestPalindrome = new LongestPalindrome();
-        System.out.println(longestPalindrome.longestPalindrome("abbb"));
+        System.out.println(longestPalindrome.longestPalindrome1("abbb"));
     }
 }
