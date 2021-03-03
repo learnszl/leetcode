f949256ad723d0c47addf87c8f8a7d309f3729e3
learnszl <ahsongzhili@163.com>
2021-03-03 17:07:07 +0800

    寻找两个正序数组的中位数

--- hard/src/FindMedianSortedArrays.java
+++ hard/src/FindMedianSortedArrays.java
@@ -1,8 +1,4 @@
-import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * @Author szl
@@ -17,33 +13,30 @@ public class FindMedianSortedArrays {
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i] < nums2[j]) {
                 list.add(nums1[i++]);
-            }
-            else if (nums1[i] > nums2[j]) {
+            } else if (nums1[i] > nums2[j]) {
                 list.add(nums2[j++]);
-            }
-            else{
+            } else {
                 list.add(nums1[i++]);
                 list.add(nums2[j++]);
             }
         }
 //        System.out.println("****" + i + "***" + j);
-        if(i == nums1.length){
+        if (i == nums1.length) {
             for (int k = j; k < nums2.length; k++) {
                 list.add(nums2[k]);
 
             }
         }
-        if(j == nums2.length){
+        if (j == nums2.length) {
             for (int k = i; k < nums1.length; k++) {
                 list.add(nums1[k]);
 
             }
         }
-        if(list.size() % 2 != 0)
+        if (list.size() % 2 != 0)
             return list.get(list.size() / 2);
         else
             return (list.get(list.size() / 2) + list.get(list.size() / 2 - 1)) / 2.0;
-
 
 
     }
@@ -51,9 +44,9 @@ public class FindMedianSortedArrays {
     public static void main(String[] args) {
         double N = 3 / 2;
         System.out.println(N);
-        int[] a = {1,3,4,5};
-        int[] b = {2,4,6,7};
-        System.out.println(findMedianSortedArrays(a,b));
+        int[] a = {1, 3, 4, 5};
+        int[] b = {2, 4, 6, 7};
+        System.out.println(findMedianSortedArrays(a, b));
 
 
     }
