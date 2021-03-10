9b587a93a50dd2a270000c89ff3056207b887f20
learnszl <ahsongzhili@163.com>
2021-03-10 10:39:03 +0800

    整数转罗马数字

---
+++ medium/src/IntegerToRoman.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @Author szl
+ * @Date 2021/3/4 21:37
+ * @Description
+ */
+public class IntegerToRoman {
+    public static String integerToRoman(int num) {
+        StringBuffer res = new StringBuffer();
+        ArrayList<Integer> temp = new ArrayList<>();
+        HashMap<Integer, String> roman = new HashMap<>() {
+            {
+                put(1, "I");
+                put(2, "II");
+                put(3, "III");
+                put(4, "IV");
+                put(5, "V");
+                put(6, "VI");
+                put(7, "VII");
+                put(8, "VIII");
+                put(9, "IX");
+                put(10, "X");
+                put(20, "XX");
+                put(30, "XXX");
+                put(40, "XL");
+                put(50, "L");
+                put(60, "LX");
+                put(70, "LXX");
+                put(80, "LXXX");
+                put(90, "XC");
+                put(100, "C");
+                put(200, "CC");
+                put(300, "CCC");
+                put(400, "CD");
+                put(500, "D");
+                put(600, "DC");
+                put(700, "DCC");
+                put(800, "DCCC");
+                put(900, "CM");
+                put(1000, "M");
+                put(2000, "MM");
+                put(3000, "MMM");
+
+            }
+        };
+        while (num > 0) {
+            temp.add(num % 10);
+            num /= 10;
+        }
+//        System.out.println(temp);
+        for (int i = temp.size() - 1; i >= 0; i--) {
+            String str = roman.get(temp.get(i) * (int) Math.pow(10, i));
+            if (str != null)
+                res.append(str);
+
+        }
+        return res.toString();
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(IntegerToRoman.integerToRoman(10));
+
+    }
+}
