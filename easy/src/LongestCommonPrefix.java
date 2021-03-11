6076f1b38213d3cae2553bbb9d2a33e035289dfa
learnszl <ahsongzhili@163.com>
2021-03-11 14:12:42 +0800

    最长公共前缀

--- easy/src/LongestCommonPrefix.java
+++ easy/src/LongestCommonPrefix.java
@@ -4,7 +4,7 @@
  * @Description
  */
 public class LongestCommonPrefix {
-    public static String longestCommonPrefix(String[] strs) {
+    public static String longestCommonPrefix(String[] strs) {//暴力
         if (strs.length == 0)
             return "";
         int N = strs.length;
@@ -34,7 +34,32 @@ public class LongestCommonPrefix {
         return res;
     }
 
+    public static String longestCommonPrefix1(String[] strs) {
+        if (strs.length == 0)
+            return "";
+        String res = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            res = findTwoCommonPrefix(res, strs[i]);
+            if (res.equals(""))
+                return "";
+        }
+        return res;
+    }
+
+    public static String findTwoCommonPrefix(String str1, String str2) {
+        StringBuilder res = new StringBuilder();
+        int len = Math.min(str1.length(), str2.length());
+        for (int i = 0; i < len; i++) {
+            if (str1.charAt(i) == str2.charAt(i))
+                res.append(str1.charAt(i));
+            else
+                break;
+        }
+        return res.toString();
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(LongestCommonPrefix.longestCommonPrefix(new String[]{"fa", "f", "fff"}));
+        System.out.println(LongestCommonPrefix.longestCommonPrefix1(new String[]{"fa", "fa", "faff"}));
     }
 }
