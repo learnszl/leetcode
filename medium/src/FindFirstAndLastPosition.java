dd9faefb74aa60b139a6b06f77e146e737994815
learnszl <ahsongzhili@163.com>
2021-03-17 12:40:04 +0800

    在排序数组中查找元素的第一个和最后一个位置

--- medium/src/FindFirstAndLastPosition.java
+++ medium/src/FindFirstAndLastPosition.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * @Description
  */
 public class FindFirstAndLastPosition {
-    public static int[] findFirstAndLastPosition(int[] nums, int target) {
+    public static int[] findFirstAndLastPosition(int[] nums, int target) {//暴力
         int i = 0, length = 0, res = 0;
         for (; i < nums.length; i++) {
             if (nums[i] == target) {
@@ -20,7 +20,43 @@ public class FindFirstAndLastPosition {
             return new int[]{-1, 1};
     }
 
+    public static int[] findFirstAndLastPosition1(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0)
+            return new int[]{-1, -1};
+        int key = binarySearch(nums, target);
+        System.out.println(key);
+        if (key == -1)
+            return new int[]{-1, -1};
+        int start = key - 1, end = key + 1;
+        for (; start >= 0; start--) {
+            if (nums[start] != nums[key])
+                break;
+        }
+        for (; end < n; end++) {
+            if (nums[end] != nums[key])
+                break;
+        }
+        return new int[]{start + 1, end - 1};
+    }
+
+    public static int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] >= target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return -1;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(findFirstAndLastPosition(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+        System.out.println(Arrays.toString(findFirstAndLastPosition1(new int[]{1, 4, 5, 6, 23, 23}, 23)));
+//        System.out.println(Arrays.toString(findFirstAndLastPosition(new int[]{5, 7, 7, 8, 8, 10}, 8)));
     }
 }
