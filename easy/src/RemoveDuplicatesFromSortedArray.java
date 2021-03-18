0f57df4b365ddb7332bf122102f42109c7201a21
learnszl <ahsongzhili@163.com>
2021-03-18 12:04:27 +0800

    删除有序数组中的重复项

--- easy/src/RemoveDuplicatesFromSortedArray.java
+++ easy/src/RemoveDuplicatesFromSortedArray.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 
 /**
  * @Author szl
@@ -9,15 +5,20 @@ import java.util.HashSet;
  * @Description
  */
 public class RemoveDuplicatesFromSortedArray {
-    public static int removeDuplicatesFromSortedArray(int[] nums){
-        int count = 0;
-        return 0;
-
-
-
-
+    public static int removeDuplicatesFromSortedArray(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j])
+                i++;
+            nums[i] = nums[j];
+        }
+        return i + 1;
     }
 
+
     public static void main(String[] args) {
+        System.out.println(removeDuplicatesFromSortedArray(new int[]{1, 1, 2, 2, 3, 4, 5}));
     }
 }
---
+++ medium/src/ValidSudoku.java
@@ -0,0 +1,13 @@
+/**
+ * @Author szl
+ * @Date 2021/3/17 17:28
+ * @Description
+ */
+public class ValidSudoku {
+    public static boolean isValidSudoku(char[][] board) {
+        boolean row = true,column = true;
+        
+        return false;
+
+    }
+}
