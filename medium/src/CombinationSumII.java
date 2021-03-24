b22777e30f6627c6ddc8875ac76b9aaabf620bcb
learnszl <ahsongzhili@163.com>
2021-03-24 16:49:19 +0800

    组合总和II

---
+++ medium/src/CombinationSumII.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author szl
+ * @Date 2021/3/24 16:34
+ * @Description
+ */
+public class CombinationSumII {
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int N = candidates.length;
+//        Arrays.sort(candidates);
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[N];
+        backtrace(res, new ArrayList<>(), 0, N, candidates, target, used);
+        return res;
+    }
+
+    public static void backtrace(List<List<Integer>> res, ArrayList<Integer> temp, int index, int len, int[] candidates, int target, boolean[] used) {
+        if (target == 0) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        if (target < 0) {
+            return;
+        }
+        for (int i = index; i < len; i++) {
+            if (!used[i]) {
+                temp.add(candidates[i]);
+                used[i] = true;
+                backtrace(res, temp, index + 1, len, candidates, target - candidates[i], used);
+                used[i] = false;
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(combinationSum2(new int[]{10, 1, 2, 7, 6, 1, 5}, 8));
+    }
+}
