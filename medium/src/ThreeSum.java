4ab916d42a2d1875be64c8b2c38878ebced9c4a2
learnszl <ahsongzhili@163.com>
2021-03-30 11:48:13 +0800

    三数之和

--- medium/src/ThreeSum.java
+++ medium/src/ThreeSum.java
@@ -29,40 +29,54 @@ public class ThreeSum {
         return new ArrayList<>(set);
     }
 
-    public static List<List<Integer>> threeSum1(int[] nums) {//虽然是双指针，但是通过hashset去重，通过但很慢
+    public static List<List<Integer>> threeSum1(int[] nums) {//虽然是双指针，但是通过hashset去重，通过但很慢,可先排序
         int N = nums.length;
         if (N < 3)
             return new ArrayList<>();
         Arrays.sort(nums);
-        HashSet<List<Integer>> res = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
         int left, right;
-        for (int i = 1; i < N - 1; i++) {
-            left = 0;
+        for (int i = 0; i < N; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            left = i + 1;
             right = N - 1;
-            while (left < i && right > i) {
-                if (nums[left] > 0)
+            while (left < right) {
+                if (nums[i] > 0)
                     break;
                 int target = nums[left] + nums[right] + nums[i];
-                if (target > 0)
-                    right--;
-                else if (target < 0)
-                    left++;
-                else {
+                if (target == 0) {
                     ArrayList<Integer> triples = new ArrayList<>();
-                    triples.add(nums[left]);
                     triples.add(nums[i]);
+                    triples.add(nums[left]);
                     triples.add(nums[right]);
                     right--;
                     left++;
                     res.add(triples);
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right + 1] == nums[right])
+                        right--;
                 }
+//                if (left > 1 && nums[left] == nums[left - 1]) {
+//                    left++;
+//                    continue;
+//                }
+//                if (right < N - 1 && nums[right] == nums[right + 1]) {
+//                    right--;
+//                    continue;
+//                }
+                else if (target > 0)
+                    right--;
+                else
+                    left++;
             }
         }
-        return new ArrayList<>(res);
+        return res;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(threeSum1(new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0}));
+        System.out.println(threeSum1(new int[]{0, 0, 0, 0}));
     }
 }
