6c6f51b452159938b01a81100998613dc2b8b772
learnszl <ahsongzhili@163.com>
2021-03-31 14:40:10 +0800

    缺失的第一个正数

--- hard/src/FirstMissingPositive.java
+++ hard/src/FirstMissingPositive.java
@@ -8,13 +8,13 @@ import java.util.HashSet;
 public class FirstMissingPositive {
     public static int firstMissingPositive(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for(int num:nums)
+        for (int num : nums)
             set.add(num);
-        System.out.println(set);
-        System.out.println(set.contains(1));
+//        System.out.println(set);
+//        System.out.println(set.contains(1));
         for (int i = 1; i < set.size() + 1; i++) {
-            if(!set.contains(i)) {
-                System.out.println(i);
+            if (!set.contains(i)) {
+//                System.out.println(i);
                 return i;
             }
         }
