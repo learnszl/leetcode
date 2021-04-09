c0a91bc1c144e83ed2593064ff981b4e91fba31d
learnszl <ahsongzhili@163.com>
2021-04-09 10:54:11 +0800

    排列序列

--- hard/src/PermutationSequence.java
+++ hard/src/PermutationSequence.java
@@ -7,23 +7,34 @@ import java.util.List;
  * @Description
  */
 public class PermutationSequence {
+    private int count = 0;
+    private final List<String> list = new ArrayList<>();
 
-    public static String getPermutation(int n, int k) {
-        ArrayList<String> res = new ArrayList<>();
+    public String getPermutation(int n, int k) {
+        List<String> res = new ArrayList<>();
         boolean[] used = new boolean[n];
-        backtrack(res, new StringBuffer(), n, used, 1);
-        return res.get(k - 1);
+        backtrack(res, new StringBuffer(), n, used, k);
+        System.out.println(res.size());
+        System.out.println(list.size());
+        System.out.println(list);
+        return res.get(0);
     }
 
-    public static void backtrack(List<String> res, StringBuffer temp, int n, boolean[] used, int count) {
-        if (temp.length() == n)
-            res.add(temp.toString());
-        else {
+    public void backtrack(List<String> res, StringBuffer temp, int n, boolean[] used, int k) {
+        if (temp.length() == n) {
+            list.add(temp.toString());
+            count++;
+            if (count == k) {
+                res.add(temp.toString());
+            }
+        } else {
             for (int i = 1; i <= n; i++) {
+                if (count >= k)
+                    continue;
                 if (!used[i - 1]) {
                     temp.append(i);
                     used[i - 1] = true;
-                    backtrack(res, temp, n, used, count + 1);
+                    backtrack(res, temp, n, used, k);
                     used[i - 1] = false;
                     temp.deleteCharAt(temp.length() - 1);
                 }
@@ -32,6 +43,7 @@ public class PermutationSequence {
     }
 
     public static void main(String[] args) {
-        System.out.println(getPermutation(4, 9));
+        PermutationSequence permutationSequence = new PermutationSequence();
+        System.out.println(permutationSequence.getPermutation(4, 9));
     }
 }
