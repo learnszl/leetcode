9d19a661a0f13bf3e5ff260d37a6fe72df6062fb
learnszl <ahsongzhili@163.com>
2021-04-12 10:17:23 +0800

    颜色分类

---
+++ medium/src/SortColors.java
@@ -0,0 +1,21 @@
+
+/**
+ * @Author szl
+ * @Date 2021/4/12 9:46
+ * @Description
+ */
+public class SortColors {
+    public static void sortColors(int[] nums) {
+
+    }
+
+
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    public static void main(String[] args) {
+    }
+}
