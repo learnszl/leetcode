77d76b7d8b356eed9b222a348499ad67571f8278
learnszl <ahsongzhili@163.com>
2021-04-13 09:50:03 +0800

    颜色分类

--- medium/src/SortColors.java
+++ medium/src/SortColors.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
 
 /**
  * @Author szl
@@ -6,16 +8,34 @@
  */
 public class SortColors {
     public static void sortColors(int[] nums) {
-
+        int N = nums.length;
+        if (N < 2)
+            return;
+        int left = 0, right = N - 1, i = 0;
+        while (i < right) {
+            if (nums[i] == 0) {
+                swap(nums, i, left);
+                left++;
+                i++;
+            } else if (nums[i] == 1)
+                i++;
+            else {
+                swap(nums, i, right);
+                right--;
+            }
+        }
     }
 
 
-    public static void swap(int a, int b) {
-        int temp = a;
-        a = b;
-        b = temp;
+    public static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 
     public static void main(String[] args) {
+        int[] nums = {2, 0, 1};
+        sortColors(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
