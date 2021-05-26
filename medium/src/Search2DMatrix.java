a435b20cf619b03eaf0d86b1a95a0284fd4df6dc
learnszl <ahsongzhili@163.com>
2021-05-26 09:43:29 +0800

    搜索二维矩阵

---
+++ medium/src/Search2DMatrix.java
@@ -0,0 +1,21 @@
+/**
+ * @Author szl
+ * @Date 2021/5/25 10:54
+ * @Description 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+ * <p>
+ * 每行中的整数从左到右按升序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ */
+public class Search2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == target)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
