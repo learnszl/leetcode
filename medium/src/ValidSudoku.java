46f5cfe6c95c5d224eb2f245080d71ac525ee613
learnszl <ahsongzhili@163.com>
2021-06-04 11:07:16 +0800

    有效的数独

--- medium/src/ValidSudoku.java
+++ medium/src/ValidSudoku.java
@@ -1,13 +1,36 @@
 /**
  * @Author szl
  * @Date 2021/3/17 17:28
- * @Description
+ * @Description 请你判断一个 9x9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。
+ * <p>
+ * 数字 1-9 在每一行只能出现一次。
+ * 数字 1-9 在每一列只能出现一次。
+ * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
+ * 数独部分空格内已填入了数字，空白格用 '.' 表示。
+ * <p>
+ * 注意：
+ * <p>
+ * 一个有效的数独（部分已被填充）不一定是可解的。
+ * 只需要根据以上规则，验证已经填入的数字是否有效即可。
  */
 public class ValidSudoku {
     public static boolean isValidSudoku(char[][] board) {
-        boolean row = true,column = true;
-        
-        return false;
-
+        boolean[][] row = new boolean[9][9];
+        boolean[][] column = new boolean[9][9];
+        boolean[][] block = new boolean[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int blockIndex = i / 3 + j / 3 * 3;
+                    if (row[i][num] || column[j][num] || block[blockIndex][num])
+                        return false;
+                    row[i][num] = true;
+                    column[j][num] = true;
+                    block[blockIndex][num] = true;
+                }
+            }
+        }
+        return true;
     }
 }
