600d076216221053a66cdc28392355a96dec1680
learnszl <ahsongzhili@163.com>
2021-06-04 17:47:09 +0800

    链表类

---
+++ src/ListNode.java
@@ -0,0 +1,22 @@
+/**
+ * @Author szl
+ * @Date 2021/6/4 17:46
+ * @Description 链表类
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
