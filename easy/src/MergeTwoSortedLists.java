a64d55b865855fb0155d28a4fbbb4b4abd6e0ea8
learnszl <ahsongzhili@163.com>
2021-06-07 11:35:36 +0800

    合并两个有序链表

--- easy/src/MergeTwoSortedLists.java
+++ easy/src/MergeTwoSortedLists.java
@@ -1,25 +1,10 @@
 /**
  * @Author szl
  * @Date 2021/3/16 21:46
- * @Description
+ * @Description 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
  */
 public class MergeTwoSortedLists {
-    static class ListNode {
-        int val;
-        ListNode next;
 
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode res = new ListNode(-1);
@@ -73,5 +58,22 @@ public class MergeTwoSortedLists {
             System.out.println(p.val);
             p = p.next;
         }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
