74289019056a5454e35d8a34bd7b469774092317
learnszl <ahsongzhili@163.com>
2021-06-11 10:14:02 +0800

    树类

---
+++ easy/src/TreeNode.java
@@ -0,0 +1,24 @@
+/**
+ * @Author szl
+ * @Date 2021/6/11 10:12
+ * @Description 定义树类
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
