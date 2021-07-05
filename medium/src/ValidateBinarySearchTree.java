56c6b2a12c41adefa7878401f82812f8eae62fc8
learnszl <ahsongzhili@163.com>
2021-07-05 20:04:37 +0800

    验证二叉搜索树

---
+++ medium/src/ValidateBinarySearchTree.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+/**
+ * @Author szl
+ * @Date 2021/7/5 19:37
+ * @Description 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 假设一个二叉搜索树具有如下特征：
+ * <p>
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class ValidateBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+        ArrayList<Integer> value = new ArrayList<>();
+        inOrder(root, value);
+        for (int i = 1; i < value.size(); i++) {
+            if (value.get(i) <= value.get(i - 1))
+                return false;
+        }
+        return true;
+    }
+
+    private void inOrder(TreeNode root, ArrayList<Integer> value) {
+        if (root == null)
+            return;
+        inOrder(root.left, value);
+        value.add(root.val);
+        inOrder(root.right, value);
+    }
+}
+
+
