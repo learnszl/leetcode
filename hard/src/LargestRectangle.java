c82facea0a2054e000ec5e2cf83a1b298c8264b8
learnszl <ahsongzhili@163.com>
2021-07-29 10:42:11 +0800

    柱状图中最大的矩形

--- hard/src/LargestRectangle.java
+++ hard/src/LargestRectangle.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 
 /**
  * @Author szl
@@ -25,7 +26,8 @@ public class LargestRectangle {
         }
         return res;
     }
-//    public static int largestRectangleArea1(int[] heights) {
+
+    //    public static int largestRectangleArea1(int[] heights) {
 //        int n = heights.length;
 //        if(n == 0)
 //            return 0;
@@ -37,9 +39,44 @@ public class LargestRectangle {
 //        }
 //
 //    }
+    public static int largestRectangleArea1(int[] heights) {//单调栈直接压入值错误，会导致无法清楚计算边界
+        int res = 0;
+        Stack<Integer> s = new Stack<>();
+//        s.push(heights[0]);
+        int[] newH = new int[heights.length + 1];
+        System.arraycopy(heights, 0, newH, 0, heights.length);
+        newH[heights.length] = 0;
+        for (int i = 0; i < newH.length; i++) {
+            int length = 0;
+            while (!s.isEmpty() && s.peek() > newH[i]) {
+                length++;
+                res = Math.max(res, s.peek() * length);
+//                System.out.println(res);
+                s.pop();
+            }
+            s.push(newH[i]);
+        }
+        return res;
+    }
+
+    public static int largestRectangleArea2(int[] heights) {
+        int res = 0;
+        Stack<Integer> s = new Stack<>();
+        int n = heights.length;
+        int[] newH = new int[n + 2];
+        System.arraycopy(heights, 0, newH, 1, n);
+        for (int i = 0; i < newH.length; i++) {
+            while (!s.isEmpty() && newH[s.peek()] > newH[i]) {
+                int curr = s.pop();
+                res = Math.max(res, (i - s.peek() - 1) * newH[curr]);
+            }
+            s.push(i);
+        }
+        return res;
+    }
 
 
     public static void main(String[] args) {
-        System.out.println(largestRectangleArea(new int[]{1}));
+        System.out.println(largestRectangleArea2(new int[]{2, 1, 2}));
     }
 }
