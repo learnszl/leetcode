e6a6da37d087d262d986db41e5822666d96883f6
learnszl <ahsongzhili@163.com>
2021-07-30 10:08:14 +0800

    子集

--- medium/src/Subsets.java
+++ medium/src/Subsets.java
@@ -4,7 +4,8 @@ import java.util.List;
 /**
  * @Author szl
  * @Date 2021/4/14 10:44
- * @Description
+ * @Description 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
  */
 public class Subsets {
     public static List<List<Integer>> subsets(int[] nums) {
