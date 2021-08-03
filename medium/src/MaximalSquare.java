3ed5143c3b48886e87b11b13be6606db27d5a372
learnszl <ahsongzhili@163.com>
2021-08-03 15:37:06 +0800

    最大正方形

--- medium/src/MaximalSquare.java
+++ medium/src/MaximalSquare.java
@@ -23,24 +23,39 @@ public class MaximalSquare {
                 new_matrix[i][j] = matrix[i][j] - '0';
             }
         }
+//        for (int i = 1; i < row; i++) {
+//            for (int j = 1; j < column; j++) {
+//                if (new_matrix[i][j] == 0 || dp[i - 1][j - 1] == 0 || dp[i][j - 1] == 0 || dp[i - 1][j] == 0) {
+//                    dp[i][j] = new_matrix[i][j];
+//                } else {
+//                    int len = (int) Math.sqrt(dp[i - 1][j - 1]), k, m;
+//                    for (k = 1; k <= len; k++) {
+//                        if (new_matrix[i][j - k] == 0)
+//                            break;
+//                    }
+//                    for (m = 1; m <= len; m++) {
+//                        if (new_matrix[i - m][j] == 0)
+//                            break;
+//                    }
+//                    int temp = Math.min(m, k);
+//                    dp[i][j] = temp * temp;
+//                }
+//                res = Math.max(res, dp[i][j]);
+//            }
+//        }
+        // 可以等价替换如下
         for (int i = 1; i < row; i++) {
             for (int j = 1; j < column; j++) {
-                if (new_matrix[i][j] == 0 || dp[i - 1][j - 1] == 0 || dp[i][j - 1] == 0 || dp[i - 1][j] == 0) {
-                    dp[i][j] = new_matrix[i][j];
-                } else {
-                    int len = (int) Math.sqrt(dp[i - 1][j - 1]), k, m;
-                    for (k = 1; k <= len; k++) {
-                        if (new_matrix[i][j - k] == 0)
-                            break;
-                    }
-                    for (m = 1; m <= len; m++) {
-                        if (new_matrix[i - m][j] == 0)
-                            break;
-                    }
-                    int temp = Math.min(m, k);
-                    dp[i][j] = temp * temp;
+                if (new_matrix[i][j] == 0)
+                    dp[i][j] = 0;
+                else {
+                    int lu = (int) Math.sqrt(dp[i - 1][j - 1]);
+                    int u = (int) Math.sqrt(dp[i - 1][j]);
+                    int l = (int) Math.sqrt(dp[i][j - 1]);
+                    int len = Math.min(lu, Math.min(u, l)) + 1;
+                    dp[i][j] = len * len;
+                    res = Math.max(res, dp[i][j]);
                 }
-                res = Math.max(res, dp[i][j]);
             }
         }
         return res;
