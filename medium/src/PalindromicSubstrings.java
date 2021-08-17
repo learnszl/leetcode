a0576b3e72cabbf6076e193cccc575d138f9e267
learnszl <ahsongzhili@163.com>
2021-08-17 18:11:38 +0800

    回文子串

--- medium/src/PalindromicSubstrings.java
+++ medium/src/PalindromicSubstrings.java
@@ -8,6 +8,8 @@
  * 解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
  **/
 public class PalindromicSubstrings {
+    int count = 0;
+
     public static int countSubstrings(String s) {
         int len = s.length(), res = 0;
         boolean[][] dp = new boolean[len][len];
@@ -19,6 +21,22 @@ public class PalindromicSubstrings {
             }
         }
         return res;
+    }
+
+    public int countSubstrings1(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            count(s, i, i);
+            count(s, i, i + 1);
+        }
+        return count;
+    }
+
+    private void count(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
+        }
     }
 
     public static void main(String[] args) {
