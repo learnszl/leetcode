8c1352ac12cc7afc6109f8354ab8bb384845a175
learnszl <ahsongzhili@163.com>
2021-09-02 11:26:58 +0800

    链表类

---
+++ medium/src/codetop/ListNode.java
@@ -0,0 +1,23 @@
+package codetop;
+
+/**
+ * @Author szl
+ * @Date 2021/9/2 10:40
+ * @Description
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
