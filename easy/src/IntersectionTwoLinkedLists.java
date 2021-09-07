5e3863f399fb1b5f35f5e75b5d617f9a7476dbdd
learnszl <ahsongzhili@163.com>
2021-09-07 15:40:37 +0800

    相交链表

---
+++ easy/src/IntersectionTwoLinkedLists.java
@@ -0,0 +1,19 @@
+/**
+ * @Author szl
+ * @Date 2021/9/7 15:22
+ * @Description 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。
+ * 如果两个链表没有交点，返回 null 。
+ * 题目数据 保证 整个链式结构中不存在环。
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ * 你能否设计一个时间复杂度 O(n) 、仅用 O(1) 内存的解决方案？
+ */
+public class IntersectionTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA, b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
