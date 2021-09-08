ff07747d2dac6d3ae2fde8c1ccd0c51730f35444
learnszl <ahsongzhili@163.com>
2021-09-08 10:53:12 +0800

    IPO

---
+++ hard/src/IPO.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @Author szl
+ * @Date 2021/9/8 10:18
+ * @Description 假设 力扣（LeetCode）即将开始 IPO 。为了以更高的价格将股票卖给风险投资公司，
+ * 力扣 希望在 IPO 之前开展一些项目以增加其资本。 由于资源有限，它只能在 IPO 之前完成最多 k 个不同的项目。
+ * 帮助 力扣 设计完成最多 k 个不同项目后得到最大总资本的方式。
+ * 给你 n 个项目。对于每个项目 i ，它都有一个纯利润 profits[i] ，和启动该项目需要的最小资本 capital[i] 。
+ * 最初，你的资本为 w 。当你完成一个项目时，你将获得纯利润，且利润将被添加到你的总资本中。
+ * 总而言之，从给定项目中选择 最多 k 个不同项目的列表，以 最大化最终资本 ，并输出最终可获得的最多资本。
+ * 答案保证在 32 位有符号整数范围内。
+ * 输入：k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
+ * 输出：4
+ * 解释：
+ * 由于你的初始资本为 0，你仅可以从 0 号项目开始。
+ * 在完成后，你将获得 1 的利润，你的总资本将变为 1。
+ * 此时你可以选择开始 1 号或 2 号项目。
+ * 由于你最多可以选择两个项目，所以你需要完成 2 号项目以获得最大的资本。
+ * 因此，输出最后最大化的资本，为 0 + 1 + 3 = 4。
+ */
+public class IPO {
+    public static int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length, res = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        int[][] project = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            project[i][0] = capital[i];
+            project[i][1] = profits[i];
+        }
+        Arrays.sort(project, (o1, o2) -> o1[0] - o2[0]);
+        System.out.println(Arrays.deepToString(project));
+        int start = 0;
+        while (k != 0) {
+            int i;
+            for (i = start; i < n; i++) {
+                if (project[i][0] <= w) {
+//                    System.out.println(project[i][1]);
+                    heap.add(project[i][1]);
+                } else
+                    break;
+            }
+            if (!heap.isEmpty())
+                w += heap.poll();
+            else
+                break;
+            start = i;
+            k--;
+        }
+        return w;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMaximizedCapital(2, 0, new int[]{1, 2, 3}, new int[]{0, 1, 1}));
+    }
+}
