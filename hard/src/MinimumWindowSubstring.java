59b04bbeb11484ba418b2d726352223443c1315d
learnszl <ahsongzhili@163.com>
2021-09-14 21:50:36 +0800

    最小覆盖子串

--- hard/src/MinimumWindowSubstring.java
+++ hard/src/MinimumWindowSubstring.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * @Author szl
  * @Date 2021/5/26 9:44
@@ -8,9 +10,49 @@
  * 输出："BANC"
  */
 public class MinimumWindowSubstring {
-    public static String minWindow(String s, String t) {
-        return "";
-        
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length())
+            return "";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int begin = 0, end = 0, counter = map.size();
+        int len = Integer.MAX_VALUE;
+        int[] pos = new int[2];
+        boolean flag = false;
+        while (end < s.length()) {
+            char c = s.charAt(end);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) == 0)
+                    counter--;
+            }
+            end++;
+            while (counter == 0) {
+                char temp = s.charAt(begin);
+                if (map.containsKey(temp)) {
+                    map.put(temp, map.get(temp) + 1);
+                    if (map.get(temp) > 0)
+                        counter++;
+                }
+                if (end - begin < len) {
+                    flag = true;
+                    pos[0] = begin;
+                    pos[1] = end;
+                    len = end - begin;
+                }
+                begin++;
+            }
+        }
+        if (flag)
+            return s.substring(pos[0], pos[1]);
+        else
+            return "";
+    }
 
+    public static void main(String[] args) {
+        MinimumWindowSubstring mws = new MinimumWindowSubstring();
+        System.out.println(mws.minWindow("ADOBECODEBANC", "ABC"));
     }
 }
